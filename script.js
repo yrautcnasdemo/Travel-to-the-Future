e69c3ee75f727e9c14f034499503f57f6f0e8f11
yrautcnasdemo <yrautcnas@msn.com>
2024-05-07 15:18:23 +0200

    responsive index ended

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+let play = document.getElementById("play");
 
-const buttons = document.querySelector(".btn-carousel");
-const slides = document.querySelector(".slide");
+function playMusic(){
+    let audio = HTMLMediaElement = url("https://youtu.be/T3mCj2u3XSY");
+    audio.play()
+}
+play.addEventListener("click", playMusic);
 
+
+
+const buttons = document.querySelectorAll(".btn-carousel");
+const slides = document.querySelectorAll(".slide");
+
+// Tableau d'image : [0, 1, 2]
 
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener("click", (e) => {
         const calcNextSlide = e.target.id === "next" ? 1 : -1;
         const slideActive = document.querySelector(".active");
 
@@ -12,9 +22,8 @@ buttons.forEach((button) => {
 
         if (newIndex < 0) newIndex = [...slides].length - 1;
         if (newIndex >= [...slides].length) newIndex = 0;
-
         slides[newIndex].classList.add("active");
+
         slideActive.classList.remove("active");
-        
     });
 });
