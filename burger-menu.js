e8e3fe10463bffa4d13daf49e73a7b7366b31aa9
JonathanACS <jonathan.beze580@gmail.com>
2024-05-17 08:45:00 +0200

    correction burger menu 2

--- burger-menu.js
+++
@@ -1,7 +0,0 @@
-//burger menu for all except index
-const menuHamburger = document.querySelector(".burger-menu");
-const navLinks = document.querySelector(".nav-list");
-
-menuHamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("mobile-menu");
-});
